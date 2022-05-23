1b318c07580c776e46c09c5a252fefb49ebbfca3
Mayankraj0410 <93092945+Mayankraj0410@users.noreply.github.com>
2022-05-23 03:15:36 +0000

    DONE

--- ActivitySet01/problem05.py
+++ ActivitySet01/problem05.py
@@ -10,3 +10,17 @@ rte = float(input("Enter rate per hour? "))
 
 p = computepay(hrs, rte)
 print("Pay", p)
+def Sum(a,b):
+    c=a+b
+    return c
+def mul(a,b):
+    f=a*b
+    return f
+a = int(input())
+b = int(input())
+
+Add =Sum(a,b)
+# print(Add)
+mul =mul(a,b)
+# print(mul)
+print(Add+mul)
