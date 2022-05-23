e2d1695ac97c7c1d996186e5a4a09e9cda82c0fa
Mayankraj0410 <93092945+Mayankraj0410@users.noreply.github.com>
2022-05-23 04:59:55 +0000

    done

--- ActivitySet01/problem05.py
+++ ActivitySet01/problem05.py
@@ -8,7 +8,7 @@ def computepay(h, r):
 hrs = float(input("Enter hours? "))
 rte = float(input("Enter rate per hour? "))
 
-p = computepay(hrs, rte)
+'''p = computepay(hrs, rte)
 print("Pay", p)
 def Sum(a,b):
     c=a+b
@@ -24,3 +24,17 @@ Add =Sum(a,b)
 mul =mul(a,b)
 # print(mul)
 print(Add+mul)
+''''
+def computepay(h, r):
+    if hrs <= 40:
+        p = hrs*rate
+    elif hrs > 40:
+        p = 40*rate+(hrs-40)*rate*1.5
+    
+    return p
+
+hrs = float(input("Enter Hours:"))
+rate =float(input("Enter rate"))
+
+p = computepay(10, 20)
+print("Pay", p)
