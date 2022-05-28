ccc314c448c5f705c8f2e9fa33e3078690567b4b
Mayankraj0410 <93092945+Mayankraj0410@users.noreply.github.com>
2022-05-28 19:31:36 +0000

    done

--- ActivitySet01/problem09.py
+++ ActivitySet01/problem09.py
@@ -1,3 +1,7 @@
 # Lists
 
 filename = "dataset/romeo.txt"
+a = int(input("Enter the number"))
+b =  int(input("Enter the number"))
+largest = a if a>b else b
+print("largest of a and b is", largest)
